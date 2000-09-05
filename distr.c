7080fbcb07c14ed4ca9f61a58aa5bc4d1c47386f
Greg Minshall <minshall@acm.org>
2000-09-05 23:13:54 +0000

    exponential works.

--- distr.c
+++ distr.c
@@ -1,26 +1,48 @@
-#include <sys/time.h>
-
+#include <ctype.h>
+#include <errno.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "distr.h"
 
 #define  NUM(a)  (sizeof (a)/(sizeof (a)[0]))
 #define  LAST(a) (&((a)[NUM(a)-1]))
 
 #define  pi() M_PI
 
+#define RANDSTATE_NBYTES        256 /* number of bytes in initstate(3) state */
 
-typedef int (*distproc_t)(int argc, char *argv[]);
+typedef struct distr distr_t, *distr_p;
+
+typedef int (*distproc_t)(distr_p distr,
+                          int iterations, distrcb_t valuecb, void *cookie,
+                          int argc, char *argv[]);
 
 
-int exponential(int argc, char *argv[]);
-int geometric(int argc, char *argv[]);
-int normal(int argc, char *argv[]);
-int pareto(int argc, char *argv[]);
-int poisson(int argc, char *argv[]);
-int uniform(int argc, char *argv[]);
+static int exponential(distr_p distr,
+                       int iterations, distrcb_t valuecb, void *cookie,
+                       int argc, char *argv[]);
+static int geometric(distr_p distr,
+                     int iterations, distrcb_t valuecb, void *cookie,
+                     int argc, char *argv[]);
+static int normal(distr_p distr,
+                  int iterations, distrcb_t valuecb, void *cookie,
+                  int argc, char *argv[]);
+static int pareto(distr_p distr,
+                  int iterations, distrcb_t valuecb, void *cookie,
+                  int argc, char *argv[]);
+static int poisson(distr_p distr,
+                   int iterations, distrcb_t valuecb, void *cookie,
+                   int argc, char *argv[]);
+static int uniform(distr_p distr,
+                   int iterations, distrcb_t valuecb, void *cookie,
+                   int argc, char *argv[]);
 
-struct distributions {
+/* mapping name to distribution functions */
+static struct distributions {
   distproc_t proc;
   char *distname;
   char *usage;
@@ -33,27 +55,61 @@ struct distributions {
   { uniform, "uniform", "[LOWER (0) UPPER (1)]" },
 };
 
-int iterations;			/* number of iterations */
-char *cmdname,			/* name of command */
-     *distname;			/* name of distribution invoked */
+/* describes a given distribution instance.  there may be multiple
+ * instances of the same distribution name ("geometric", say); each
+ * has its own random number space feeding it, so should be independent.
+ */
+
+struct distr {
+    distproc_t proc;
+    char randstate[RANDSTATE_NBYTES];
+};
+
 
 /********************
  * Utility routines *
  ********************/
 
-double
+
+static int
+getdouble(char *string, double *result) {
+    char *endptr;
+
+    errno = 0;
+
+    *result = strtod(string, &endptr);
+
+    if ((*result == 0) && (errno == ERANGE)) {
+        return 1;
+    }
+
+    if ((*result == HUGE_VAL) && (errno == ERANGE)) {
+        return 1;
+    }
+
+    if (*endptr == 0) {
+        return 0;
+    } else if (isspace((int)*endptr)) {
+        return 0;
+    } else {
+        return EINVAL;
+    }
+}
+
+
+static double
 unitrectangular(void)
 {
-  long lr;
-  double r;
+    long lr;
+    double r;
 
-  lr = random();
-  if (lr) {
-    r = (lr*1.0)/(0x7fffffff-1.0);
-  } else {
-    r = 0.0;
-  }
-  return r;
+    lr = random();
+    if (lr) {
+        r = (lr*1.0)/(0x7fffffff-1.0);
+    } else {
+        r = 0.0;
+    }
+    return r;
 }
 
 
@@ -62,309 +118,277 @@ unitrectangular(void)
  *****************/
 
 
-int
-exponential(int argc, char *argv[])
+static int exponential(distr_p distr,
+                       int iterations, distrcb_t valuecb, void *cookie,
+                       int argc, char *argv[])
 {
-  double ans, b;
+    double ans, b;
 
-  b = 1.0;
+    b = 1.0;
 
-  if (argc > 0) {
-    b = atof(argv[0]);
-    if (b < 0) {
-      fprintf(stderr, "invalid mean %g; must be >= 0\n", b);
-      return 1;
-    }
-  }
-
-  if (argc > 1) {
-    fprintf(stderr, "too many arguments to exponential\n");
-    return 1;
-  }
-
-  while (iterations--) {
-    ans = - ( b * log(unitrectangular()) );
-    printf("%g\n", ans);
-  }
-
-  return 0;
-}
-
-int
-geometric(int argc, char *argv[])
-{
-  double ans, p, log1mp;
-
-  p = 0.5;
-
-  if (argc > 0) {
-    p = atof(argv[0]);
-    if ((p < 0) || (p > 1.0)) {
-      fprintf(stderr, "invalid value %g for p; must be 1.0 >= p >= 0\n", p);
-      return 1;
-    }
-  }
-
-  if (argc > 1) {
-    fprintf(stderr, "too many parameters to geometric\n");
-    return 1;
-  }
-
-  log1mp = log(1-p);
-
-  while (iterations--) {
-    ans = (log(unitrectangular())/log1mp)-1;
-    ans = ceil(ans);
-    printf("%g\n", ans);
-  }
-
-  return 0;
-}
-
-
-int
-normal(int argc, char *argv[])
-{
-  double mean, stddev, ans;
-
-  mean = 0.0;
-  stddev = 1.0;
-
-  if (argc) {
-    mean = atof(argv[0]);
     if (argc > 1) {
-      stddev = atof(argv[1]);
-      if (stddev <= 0) {
-	fprintf(stderr, "invalid standard deviation %g; must be > 0\n",
-		stddev);
-      }
-      if (argc > 2) {
-	fprintf(stderr, "too many parameters to normal\n");
-	return 1;
-      }
+        if (getdouble(argv[1], &b)) {
+            fprintf(stderr,
+                  "%s: invalid mean ``%s''; must be a floating point number\n",
+                    argv[0], argv[1]);
+            return 1;
+        }
+        if (b < 0) {
+            fprintf(stderr, "%s: invalid mean %g; must be >= 0\n",
+                    argv[0], b);
+            return 1;
+        }
     }
-  }
 
-  while (iterations--) {
-    ans = sqrt(-2*log(unitrectangular())) * sin(2*pi()*unitrectangular());
-    ans = (stddev * ans) + mean;
-    printf("%g\n", ans);
-  }
+    if (argc > 2) {
+        fprintf(stderr, "%s: too many arguments\n", argv[0]);
+        return 1;
+    }
 
-  return 0;
+    while (iterations--) {
+        ans = - ( b * log(unitrectangular()) );
+        printf("%g\n", ans);
+    }
+
+    return 0;
+}
+
+static int geometric(distr_p distr,
+                     int iterations, distrcb_t valuecb, void *cookie,
+                     int argc, char *argv[])
+{
+    double ans, p, log1mp;
+
+    p = 0.5;
+
+    if (argc > 0) {
+        p = atof(argv[0]);
+        if ((p < 0) || (p > 1.0)) {
+            fprintf(stderr, "invalid value %g for p; must be 1.0 >= p >= 0\n", p);
+            return 1;
+        }
+    }
+
+    if (argc > 1) {
+        fprintf(stderr, "too many parameters to geometric\n");
+        return 1;
+    }
+
+    log1mp = log(1-p);
+
+    while (iterations--) {
+        ans = (log(unitrectangular())/log1mp)-1;
+        ans = ceil(ans);
+        printf("%g\n", ans);
+    }
+
+    return 0;
+}
+
+
+static int normal(distr_p distr,
+                  int iterations, distrcb_t valuecb, void *cookie,
+                  int argc, char *argv[])
+{
+    double mean, stddev, ans;
+
+    mean = 0.0;
+    stddev = 1.0;
+
+    if (argc) {
+        mean = atof(argv[0]);
+        if (argc > 1) {
+            stddev = atof(argv[1]);
+            if (stddev <= 0) {
+                fprintf(stderr, "invalid standard deviation %g; must be > 0\n",
+                        stddev);
+            }
+            if (argc > 2) {
+                fprintf(stderr, "too many parameters to normal\n");
+                return 1;
+            }
+        }
+    }
+
+    while (iterations--) {
+        ans = sqrt(-2*log(unitrectangular())) * sin(2*pi()*unitrectangular());
+        ans = (stddev * ans) + mean;
+        printf("%g\n", ans);
+    }
+
+    return 0;
 }
 					      
     
-int
-pareto(int argc, char *argv[])
+static int pareto(distr_p distr,
+                  int iterations, distrcb_t valuecb, void *cookie,
+                  int argc, char *argv[])
 {
-  double a, c, crecip, ans;
+    double a, c, crecip, ans;
 
-  a = 1;
-  c = 1;
+    a = 1;
+    c = 1;
 
-  if (argc) {
-    a = atof(argv[0]);
-    if (a <= 0) {
-      fprintf(stderr, "invalid location %g; must be > 0\n", a);
-      return 1;
+    if (argc) {
+        a = atof(argv[0]);
+        if (a <= 0) {
+            fprintf(stderr, "invalid location %g; must be > 0\n", a);
+            return 1;
+        }
+        if (argc > 1) {
+            c = atof(argv[1]);
+            if (c <= 0) {
+                fprintf(stderr, "invalid shape %g; must be > 0\n", c);
+                return 1;
+            }
+            if (argc > 2) {
+                fprintf(stderr, "too many parameters for pareto\n");
+                return 1;
+            }
+        }
     }
-    if (argc > 1) {
-      c = atof(argv[1]);
-      if (c <= 0) {
-	fprintf(stderr, "invalid shape %g; must be > 0\n", c);
-	return 1;
-      }
-      if (argc > 2) {
-	fprintf(stderr, "too many parameters for pareto\n");
-	return 1;
-      }
+
+    crecip = 1/c;
+
+    while (iterations--) {
+        ans = a * pow(1.0-unitrectangular(), -crecip);
+        printf("%g\n", ans);
     }
-  }
 
-  crecip = 1/c;
-
-  while (iterations--) {
-    ans = a * pow(1.0-unitrectangular(), -crecip);
-    printf("%g\n", ans);
-  }
-
-  return 0;
+    return 0;
 }
 
 
-int
-poisson(int argc, char *argv[])
+static int poisson(distr_p distr,
+                   int iterations, distrcb_t valuecb, void *cookie,
+                   int argc, char *argv[])
 {
-  double lambda, r, partial, sum;
-  int i;
+    double lambda, r, partial, sum;
+    int i;
 
-  lambda = 1.0;
+    lambda = 1.0;
 
-  if (argc) {
-    lambda = atof(argv[0]);
-    if (lambda <= 0) {
-      fprintf(stderr, "invalid mean %g; must be > 0\n", lambda);
-      return 1;
+    if (argc) {
+        lambda = atof(argv[0]);
+        if (lambda <= 0) {
+            fprintf(stderr, "invalid mean %g; must be > 0\n", lambda);
+            return 1;
+        }
+        if (argc > 1) {
+            fprintf(stderr, "too many parameters for poisson\n");
+            return 1;
+        }
     }
-    if (argc > 1) {
-      fprintf(stderr, "too many parameters for poisson\n");
-      return 1;
-    }
-  }
 
-  while (iterations--) {
-    r = unitrectangular();
-    partial = exp(-lambda);
-    sum = partial;
-    if (r < sum) {
-      printf("0\n");
-      continue;
+    while (iterations--) {
+        r = unitrectangular();
+        partial = exp(-lambda);
+        sum = partial;
+        if (r < sum) {
+            printf("0\n");
+            continue;
+        }
+        i = 0;
+        while (1) {
+            i++;
+            partial = (lambda * partial) / i;
+            sum = sum+partial;
+            if (r < sum) {
+                printf("%d\n", i);
+                break;
+            }
+        }
     }
-    i = 0;
-    while (1) {
-      i++;
-      partial = (lambda * partial) / i;
-      sum = sum+partial;
-      if (r < sum) {
-	printf("%d\n", i);
-	break;
-      }
-    }
-  }
-  return 0;
+    return 0;
 }
 
 
-int
-uniform(int argc, char *argv[])
+static int uniform(distr_p distr,
+                   int iterations, distrcb_t valuecb, void *cookie,
+                   int argc, char *argv[])
 {
-  double a, b, len, ans;
+    double a, b, len, ans;
 
-  a = 0.0;
-  b = 1.0;
+    a = 0.0;
+    b = 1.0;
 
-  if (argc > 0) {
-    if (argc != 2) {
-      fprintf(stderr,
-	      "uniform must have no parameters or exactly two parameters\n");
-      return 1;
+    if (argc > 0) {
+        if (argc != 2) {
+            fprintf(stderr,
+                    "uniform must have no parameters or exactly two parameters\n");
+            return 1;
+        }
+        a = atof(argv[0]);
+        b = atof(argv[1]);
+        if (b <= a) {
+            fprintf(stderr, "invalid LOWER %g UPPER %g; must be LOWER < UPPER\n",
+                    a, b);
+            return 1;
+        }
     }
-    a = atof(argv[0]);
-    b = atof(argv[1]);
-    if (b <= a) {
-      fprintf(stderr, "invalid LOWER %g UPPER %g; must be LOWER < UPPER\n",
-	      a, b);
-      return 1;
+
+    len = b-a;
+    while (iterations--) {
+        ans = a + (len*unitrectangular());
+        printf("%g\n", ans);
     }
-  }
 
-  len = b-a;
-  while (iterations--) {
-    ans = a + (len*unitrectangular());
-    printf("%g\n", ans);
-  }
-
-  return 0;
+    return 0;
 }
 
 
 
-/**********************
- * Overhead routines  *
- **********************/
+static distr_p newdistr(void) {
+    distr_p distr;
 
-static void
-help(void)
-{
-  struct distributions *dist;
+    distr = (distr_p) malloc(sizeof *distr);
+    if (distr == 0) {
+        return 0;
+    }
+    bzero(distr, sizeof *distr);
 
-  fprintf(stderr, "Distributions:\n");
-
-  for (dist = distributions; dist <= LAST(distributions); dist++) {
-    fprintf(stderr, "\t%s %s\n", dist->distname, dist->usage);
-  }
-  exit(1);
+    return distr;
 }
 
 
-static void
-usage(void)
+void
+distrhelp(void)
 {
-  fprintf(stderr,
-	  "usage: %s [-h] [-i ITERATIONS] [-s SEED] DISTRIBUTION PARAMETERS\n",
-	  cmdname);
-  exit(1);
+    struct distributions *dist;
+
+    fprintf(stderr, "Distributions:\n");
+
+    for (dist = distributions; dist <= LAST(distributions); dist++) {
+        fprintf(stderr, "\t%s %s\n", dist->distname, dist->usage);
+    }
 }
 
-int
-main(int argc, char *argv[])
-{
-  int opt;
-  int sflag;
-  unsigned soption;
-  struct distributions *dist;
-  struct timeval tv;
-  extern char *optarg;
-  extern int optind, optreset;
 
-  cmdname = argv[0];
+void *
+distrnew(char *name, unsigned long seed) {
+    distr_p distr;
+    int i;
 
-  sflag = 0;
-  iterations = 1;
-
-  optreset = 1;
-  optind = 1;
-
-  while ((opt = getopt(argc, argv, "hi:s:")) != EOF) {
-    switch (opt) {
-    case 'h':
-      help();
-      /*NOTREACHED*/
-      break;
-    case 'i':
-      iterations = atoi(optarg);
-      break;
-    case 's':
-      soption = atoi(optarg);
-      sflag = 1;
-      break;
-    default:
-      usage();
-      /*NOTREACHED*/
+    for (i = 0; i < NUM(distributions); i++) {
+        if (strcmp(distributions[i].distname, name) == 0) {
+            distr = newdistr();
+            if (distr == 0) {
+                return 0;
+            }
+            distr->proc = distributions[i].proc;
+            initstate(seed, distr->randstate, sizeof distr->randstate);
+            return distr;
+        }
     }
-  }
+    /* didn't find it */
+    return 0;
+}
 
-  argc -= optind;
-  argv += optind;
 
-	    
-	
-  if (argc == 0) {
-    usage();
-    /*NOTREACHED*/
-  }
+int distr(void *context, int iterations,
+           distrcb_t valuecb, void *cookie, int argc, char *argv[]) {
+    distr_p distr = (distr_p)context;
 
-  /* initialize random number generator */
-  if (sflag) {
-    (void)srandom(soption);
-  } else {
-    gettimeofday(&tv, 0);
-    (void)srandom(tv.tv_usec ^ (tv.tv_usec>>16));
-  }
-
-  distname = argv[0];
-  argc--;
-  argv++;
-
-  for (dist = distributions; dist <= LAST(distributions); dist++) {
-    if (!strcmp(distname, dist->distname)) {
-      return dist->proc(argc, argv);
-    }
-  }
-
-  fprintf(stderr, "unknown distribution \"%s\"\n", distname);
-  usage();
-  /*NOTREACHED*/
-  return 0;
+    (void) setstate(distr->randstate);
+    return distr->proc(distr, iterations, valuecb, cookie, argc, argv);
 }
